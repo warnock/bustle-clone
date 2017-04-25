cfa9b58e0c11847a781939279002f984f8a2526b
Megan Warnock <megandwarnock@gmail.com>
2017-04-25 16:26:41 -0700

    add dynamic rounting to story

--- app/router.js
+++ app/router.js
@@ -9,7 +9,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('about');
   this.route('admin');
-  this.route('story');
+  this.route('story', {path: '/story/:story_id'});
 });
 
 export default Router;
